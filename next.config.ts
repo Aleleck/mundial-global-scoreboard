a2aba9b777cdef6fda0ef84eead95a7f2451702f
Aleleck <panchaponal18@gmail.com>
2025-12-15 17:31:21 -0500

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "export", // Cambia de "standalone" a "export"
+  basePath: "/mundial-global-scoreboard", // Reemplaza con el nombre de tu repo
+  assetPrefix: "/mundial-global-scoreboard", // Mismo nombre
+  images: {
+    unoptimized: true, // GitHub Pages no soporta optimización de imágenes
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
